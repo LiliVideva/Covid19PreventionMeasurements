ea1c5df5b451cc62260cb01473a20d57696b1349
Stefan Stamenov <stefan.stamenov1996@gmail.com>
2020-05-19 00:37:33 +0300

    Refactoring date-sentences to be in format {page_title: {date,sentence}}, because we want to label the plot of every country. Adding temporary data_sentences file, till refactoring and connect to the pipe in the extraction.py

--- map_date_sentence.py
+++ map_date_sentence.py
@@ -5,11 +5,11 @@ nlp = spacy.load("en_core_web_sm")
 
 
 def get_event_by_date(page_contents, relevant_section_titles):
-    date_sentences = {}
+    pages_date_sentences = {}
 
     for page_title, page_content in page_contents.items():
+        date_sentence = {}
         page_relevant_sections = [key for key in page_content.keys() if key in relevant_section_titles]
-
         for section in page_relevant_sections:
             for line in page_content[section]:
                 doc = nlp(line)
@@ -19,12 +19,10 @@ def get_event_by_date(page_contents, relevant_section_titles):
                 for sent in doc.sents:
                     for d in dates:
                         if d in sent.text:
-                            date_sentences[reformat_date(d)] = sent.text
-            # print(f'{section}:\n'
-            #       f'========================\n')
-            # for key, value in date_sentences.items():
-            #     print(f'"{key}" - "{value}"')
-    return date_sentences
+                            date_sentence[reformat_date(d)] = sent.text
+
+        pages_date_sentences[page_title] = date_sentence
+    return pages_date_sentences
 
 
 def reformat_date(dt):
